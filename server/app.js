b2ae2d36ef96e66f0dbe1b98970281624c8e9a1e
Ian Mobley <ianmobley@gmail.com>
2017-09-26 10:06:28 -0700

    add ejs, index view, and route

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('*', (req, res) => {
+  res.render('index');
+})
 
 module.exports = app;
